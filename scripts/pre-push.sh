95c9b3d0305f4149f8e1df888e0ef1ad41a3efb1
dkje <developer.kje@gmail.com>
2020-11-30 12:11:27 +0900

    [RE-67] | push-hook test

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -8,6 +8,7 @@ BRANCH_NAME=$(git symbolic-ref --short HEAD)
 # BRANCH_NAME="${BRANCH_NAME##*/}"
 
 JIRA_ID=$(echo $BRANCH_NAME)
+echo "JIRA_ID: $JIRA_ID"
 
 if [ $1 = "upstream" ]; then
   echo "upstream에 push 하지 마시오!👿"
