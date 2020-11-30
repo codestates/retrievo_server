f5fc9d8f8c72e0b038a13a05813870908707e4f7
dkje <developer.kje@gmail.com>
2020-11-30 14:19:28 +0900

    [RE-67] | add pakage-lock.json to gitignore

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,12 +5,3 @@ echo ""
 echo "(Do you want to commit your works? pre-commit test start!}🐶"
 
 lint-staged
-
-git diff --cached --name-status | while read x file; do
-  for disallowed in package-lock.json .env; do
-    if [ "$disallowed" == "$file" ]; then
-      echo "ERROR: This commit contains disallowed file: \"${disallowed}\""
-      exit 1
-    fi
-  done
-done || exit $?
