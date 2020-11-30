82136b845aa9c646c5a586f41375e4e730351a13
dkje <developer.kje@gmail.com>
2020-11-30 12:16:10 +0900

    [RE-67] | push test

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -2,15 +2,9 @@
 echo ""
 echo "(more check before push}🐶"
 
-echo "1: $1"
+echo "push to: $1"
 
-BRANCH_NAME=$(git symbolic-ref --short HEAD)
-# BRANCH_NAME="${BRANCH_NAME##*/}"
-
-JIRA_ID=$(echo $BRANCH_NAME)
-echo "JIRA_ID: $JIRA_ID"
-
-if [ $1 = "upstream" ]; then
+if [ $1 = "origin" ]; then
   echo "upstream에 push 하지 마시오!👿"
   exit 1
 fi
