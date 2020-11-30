eb1fb83d07b5531ffbb31c61dcd3057156329274
dkje <developer.kje@gmail.com>
2020-11-30 14:14:06 +0900

    [RE-67] | block upload env & pakagelock test

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,16 @@
+#!/bin/sh
+
 echo ""
 echo ""
 echo "(Do you want to commit your works? pre-commit test start!}🐶"
+
 lint-staged
+
+git diff --cached --name-status | while read x file; do
+  for disallowed in .env package-lock.json; do
+    if [ "$disallowed" == "$file" ]; then
+      echo "ERROR: This commit contains disallowed file: \"${disallowed}\""
+      exit 1
+    fi
+  done
+done || exit $?
