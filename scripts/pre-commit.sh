a4e5f77f470ac7edbf635e4d08d9800f72d23631
dkje <developer.kje@gmail.com>
2020-11-30 14:17:00 +0900

    [RE-67] | block upload env

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -7,7 +7,7 @@ echo "(Do you want to commit your works? pre-commit test start!}🐶"
 lint-staged
 
 git diff --cached --name-status | while read x file; do
-  for disallowed in .env package-lock.json; do
+  for disallowed in package-lock.json; do
     if [ "$disallowed" == "$file" ]; then
       echo "ERROR: This commit contains disallowed file: \"${disallowed}\""
       exit 1
