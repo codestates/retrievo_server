3c58d30c437c59b698bd366f499642beea4e492d
dkje <developer.kje@gmail.com>
2020-12-01 11:52:20 +0900

    [RE-117] | change SocialLogin module name

---
+++ src/entities/index.ts
@@ -0,0 +1,7 @@
+import User from "./User";
+import SocialLogin from "./SocialLogins";
+import Project from "./Project";
+import ProjectPermission from "./ProjectPermission";
+
+const entities = [User, SocialLogin, Project, ProjectPermission];
+export default entities;
