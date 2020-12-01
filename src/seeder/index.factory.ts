2c20eccc8d0dc05acc366297353022c57e6cfccf
dkje <developer.kje@gmail.com>
2020-12-02 00:14:44 +0900

    [RE-117] | add seed for Task & Board & Sprint

--- src/seeder/index.factory.ts
+++ src/seeder/index.factory.ts
@@ -1,11 +1,19 @@
 import { define } from "typeorm-seeding";
 import Faker from "faker";
+import _ from "lodash";
 
 // entities
 import User, { roleTypes } from "../entities/User";
 import Project from "../entities/Project";
+import Task from "../entities/Task";
 import ProjectPermission from "../entities/ProjectPermission";
 import SocialLogin, { socialProviderType } from "../entities/SocialLogins";
+import Comment from "../entities/Comment";
+import Sprint from "../entities/Sprint";
+import Action, { actionType } from "../entities/Action";
+import Board from "../entities/Board";
+import Label from "../entities/Label";
+import File from "../entities/File";
 
 define(User, (faker: typeof Faker) => {
   const roles = [roleTypes.MEMBER, roleTypes.GUEST, roleTypes.ADMIN];
@@ -15,7 +23,7 @@ define(User, (faker: typeof Faker) => {
   user.username = faker.name.findName();
   user.email = faker.internet.email();
   user.password = faker.random.word();
-  user.role = roles[Math.floor(Math.random() * 2 + 1)];
+  user.role = roles[Math.floor(Math.random() * 3)];
   return user;
 });
 
@@ -25,7 +33,7 @@ define(SocialLogin, (faker: typeof Faker) => {
       socialProviderType.GITHUB,
       socialProviderType.GOOGLE,
     ];
-    return socialProviderArray[Math.floor(Math.random())];
+    return socialProviderArray[Math.floor(Math.random() * 2)];
   };
 
   const socialLogin = new SocialLogin();
@@ -50,25 +58,68 @@ define(Project, (faker: typeof Faker) => {
   return project;
 });
 
-//   export default class CreatePets implements Seeder {
-//   public async run(factory: Factory, connection: Connection): Promise<any> {
+define(Action, (faker: typeof Faker) => {
+  const types = [
+    actionType.TASK_ASSIGNMENT,
+    actionType.TASK_STATUS_CHANGE,
+    actionType.COMMENT_NEW,
+    actionType.SPRINT_START,
+    actionType.SPRINT_END,
+  ];
 
-//     await factory(User)().map(async (user: User) => {
-//     const socialLogins: Users[] = await factory(User)().createMany(5);
-//     const socialProviderIds = users.map((user: User) => user.Id);
-//     await Project.user().attach(userIds);
+  const action = new Action();
+  action.id = faker.random.uuid();
+  action.description = types[_.random(0, 4)];
+  return action;
+});
 
-// //
-// await factory(ProjectPermission)()
-//   .map(async (projectPermission: Project) => {
-//     const users: Users[] = await factory(User)().createMany(5);
-//     const userIds = users.map((user: User) => user.Id);
-//     const projectIds = users.map(());
-//     const socialLogin: afacas~
-//
-//     await Projectpermisison.user().attach(userIds);
-//     await Projectpermisison.project().attac(usdf);
-//   })
-//   .createMany(5);
-// // project Many(5);
-// // project
+define(Task, (faker: typeof Faker) => {
+  const task = new Task();
+  task.id = faker.random.uuid();
+  task.title = faker.random.words();
+  task.description = faker.lorem.sentence();
+  task.startDate = faker.date.past();
+  task.endDate = faker.date.future();
+  return task;
+});
+
+define(Comment, (faker: typeof Faker) => {
+  const comment = new Comment();
+  comment.id = faker.random.uuid();
+  comment.rootCommentId = faker.random.uuid();
+  comment.content = faker.random.words();
+  return comment;
+});
+
+define(Sprint, (faker: typeof Faker) => {
+  const sprint = new Sprint();
+  sprint.id = faker.random.uuid();
+  sprint.title = faker.random.words();
+  sprint.description = faker.lorem.sentence();
+  sprint.boardRow = faker.random.number();
+  sprint.startedAt = faker.date.recent();
+  sprint.dueDate = faker.date.future();
+  return sprint;
+});
+
+define(File, (faker: typeof Faker) => {
+  const file = new File();
+  file.id = faker.random.uuid();
+  file.fileLink = faker.system.fileName();
+  return file;
+});
+
+define(Label, (faker: typeof Faker) => {
+  const label = new Label();
+  label.id = faker.random.uuid();
+  label.name = faker.random.word();
+  label.color = faker.commerce.color();
+  return label;
+});
+
+define(Board, (faker: typeof Faker) => {
+  const board = new Board();
+  board.id = faker.random.uuid();
+  board.title = faker.random.words();
+  return board;
+});
--- src/seeder/index.seed.ts
+++ src/seeder/index.seed.ts
@@ -1,31 +1,112 @@
+/* eslint-disable */
+
 import { Factory, Seeder } from "typeorm-seeding";
 // entities
 import User from "../entities/User";
 import Project from "../entities/Project";
 import ProjectPermission from "../entities/ProjectPermission";
 import SocialLogin from "../entities/SocialLogins";
+// import Action from "../entities/Action";
+import Task from "../entities/Task";
+import Sprint from "../entities/Sprint";
+// import Comment from "../entities/Comment";
+import Board from "../entities/Board";
+import Label from "../entities/Label";
+// import File from "../entities/File";
 
 export default class CreateSeeds implements Seeder {
   async run(factory: Factory): Promise<void> {
+    const MAXNUM = 30; //반드시 30단위어야한다
+
     const users = await factory(User)()
       .map(async (user: User) => {
         const socialLogin = await factory(SocialLogin)().create();
+        // NOTE socialLogin created;
         return Object.assign(user, { socialLogin });
       })
-      .createMany(30);
+      .createMany(MAXNUM); // NOTE user created;
 
-    const projects = await factory(Project)().createMany(10);
-    const projectPermissions = await factory(ProjectPermission)().makeMany(30);
+    const projects = await factory(Project)().createMany(MAXNUM / 3);
+    // NOTE project created;
+
+    const projectPermissions = await factory(ProjectPermission)().makeMany(
+      MAXNUM
+    );
 
     let index = 0;
 
-    // eslint-disable-next-line no-restricted-syntax
     for await (const projectPermission of projectPermissions) {
       const user = users[index];
       const project = projects[index % 10];
       Object.assign(projectPermission, { user, project });
       await factory(ProjectPermission)().create(projectPermission);
       index += 1;
+    } // NOTE projectPermission created
+
+    // 생각 정리
+    // task는 sprint랑 board를 갖고 있음
+    // 근데 sprint랑 board는 똑같은 project를 갖고 있어야 함
+
+    index = 0;
+    const sprintIds = []; // 30
+    const sprints = await factory(Sprint)().makeMany(MAXNUM);
+    const labels = await factory(Label)().makeMany(MAXNUM);
+    // const task = await factory(Task)().makeMany(MAXNUM * 10);
+    for await (const sprint of sprints) {
+      sprintIds.push(sprint.id);
+      const label = labels[index];
+      const project = projects[index % 10]; //1 ~ 10
+      const boardRow = Math.floor(index / 10);
+      Object.assign(sprint, { project, boardRow });
+      Object.assign(label, { project });
+      await factory(Sprint)().create(sprint);
+      await factory(Label)().create(label);
+      index += 1;
+    } // NOTE Sprint created
+
+    index = 0;
+    const boardIds = []; // 90
+    const boards = await factory(Board)().makeMany(MAXNUM * 3);
+    for await (const board of boards) {
+      boardIds.push(board.id);
+      const project = projects[index % 10];
+      const boardColumnIndex = Math.floor(index / 10);
+      Object.assign(board, { project, boardColumnIndex });
+      await factory(Board)().create(board);
+      index += 1;
+    } // NOTE Board created
+
+    index = 0;
+    const taskIds = [];
+    const tasks = await factory(Task)().makeMany(MAXNUM * 9);
+    for await (const task of tasks) {
+      taskIds.push(task.id);
+      const sprint = sprintIds[Math.floor(index % 30)];
+      const board = boardIds[Math.floor(index % 90)];
+      const boardRowIndex = Math.floor(index / 90);
+      const sprintRowIndex = Math.floor(index / 30);
+      Object.assign(task, { sprint, board, boardRowIndex, sprintRowIndex });
+      await factory(Task)().create(task);
+      index += 1;
     }
+
+    // index = 0;
+    // const files = await factory(File)().makeMany(MAXNUM * 10);
+    // for await (const file of files) {
+    //   const task = tasks[Math.floor(index / 2)];
+    //   Object.assign(file, { task });
+    //   await factory(File)().create(file);
+    //   index += 1;
+    // }
+
+    // TODO file
+    // TODO comments
+    // TODO task_label
+    // TODO user_task ㅜㅜ
+    // TODO actions
+
+    /*
+    action에는 task, sprint, user, project, comment가 들어간다 -> forLoop을 돌리는 친구
+    */
   }
 }
