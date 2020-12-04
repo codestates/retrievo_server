940ad7864ef049530da315ed75c9ac3c08aa4b75
dkje <developer.kje@gmail.com>
2020-12-04 16:45:49 +0900

    [RE-128] | change socialLogin validations

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -3,10 +3,7 @@ import { MiddlewareFn } from "type-graphql";
 import { MyContext } from "../resolvers/types/MyContext";
 
 const checkAuthStatus: MiddlewareFn<MyContext> = async ({ context }, next) => {
-  console.log("context.req.session", context.req.session);
   const currentUser = context.req.session.passport?.user;
-  console.log("나 김정은이야!");
-  console.log("currentUser:", currentUser);
   if (!currentUser)
     throw new AuthenticationError("Please login to access to the service 😱");
   return await next();
