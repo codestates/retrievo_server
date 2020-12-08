f796656082d6c1324e74acc2f10d66af7cce149d
pbkim0725 <pbkim0725@gmail.com>
2020-12-08 22:30:26 +0900

    [RE-172] | [Added]|Reset board row index when started/completed

---
+++ src/utils/AWSS3Uploader.ts
@@ -0,0 +1,7 @@
+// import { AWSS3Uploader } from "./s3";
+
+// const s3Uploader = new AWSS3Uploader({
+//   accessKeyId: process.env.AWS_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   destinationBucketName: "my-really-cool-bucket",
+// });
