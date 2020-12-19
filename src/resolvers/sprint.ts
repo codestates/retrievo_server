7d5cc46020e69ca4f2a9fe81c69b8b7e7a62bfa6
pbkim0725 <pbkim0725@gmail.com>
2020-12-19 19:41:26 +0900

    [sprint] | sprint delete fixed

--- src/resolvers/sprint.ts
+++ src/resolvers/sprint.ts
@@ -1,7 +1,7 @@
 import { Resolver, Arg, Query, Mutation, UseMiddleware } from "type-graphql";
 
 /* Entities */
-import { getConnection, getRepository } from "typeorm";
+import { getConnection, getManager, getRepository } from "typeorm";
 import Board from "../entities/Board";
 import Sprint from "../entities/Sprint";
 import SprintNotification, {
@@ -94,15 +94,12 @@ export class SprintResolver {
     if (!projectId) {
       return { error: generateError(errorKeys.DATA_NOT_FOUND) };
     }
-
     try {
       const sprints = await Sprint.find({
         where: { project: projectId },
         relations: ["project"],
       });
-
-      const row = sprints.length ? sprints.length - 1 : 0;
-
+      const row = sprints.length ? sprints.length : 0;
       const sprint = await Sprint.create({
         title,
         description,
@@ -248,17 +245,28 @@ export class SprintResolver {
     @Arg("id") id: string
   ): Promise<SprintResponse> {
     const sprint = await Sprint.findOne(id);
+    const em = getManager();
+    const sprints = await em.find(Sprint, {
+      where: { project: projectId },
+      relations: ["project"],
+    });
 
-    if (!sprint) {
+    if (!sprint || !sprints) {
       return { error: generateError(errorKeys.DATA_NOT_FOUND) };
     }
 
+    const newSprints = sprints.filter((item) => item.id !== sprint.id);
+    const reorderedSprints = newSprints.map((item, index) => {
+      Object.assign(item, { row: index });
+      return item;
+    });
+
     try {
       await Sprint.delete(id);
+      await em.save(reorderedSprints);
       return { success: true };
     } catch (err) {
       console.log("projectId", projectId);
-      console.log(err);
       return { error: generateError(errorKeys.INTERNAL_SERVER_ERROR) };
     }
   }
@@ -266,7 +274,7 @@ export class SprintResolver {
   @Mutation(() => SprintResponse)
   @UseMiddleware([checkAuthStatus, checkProjectPermission])
   async readSprintNotification(
-    @Arg("projectId") projectId: string,
+    // @Arg("projectId") projectId: string,
     @Arg("id") id: string
   ): Promise<SprintResponse> {
     const sprintNotification = await SprintNotification.findOne(id);
@@ -279,7 +287,6 @@ export class SprintResolver {
       await SprintNotification.update(sprintNotification.id, { isRead: true });
       return { success: true };
     } catch (err) {
-      console.log("projectId", projectId);
       return { error: generateError(errorKeys.INTERNAL_SERVER_ERROR) };
     }
   }
